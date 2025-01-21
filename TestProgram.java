7564d7013e444b6d3391c947c9700c58487de86f
Joshua Reynolds <reynjs21@wfu.edu>
2025-01-21 11:12:18 -0500

    First Commit and Push

--- TestProgram.java
+++ TestProgram.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
-public static void main(string[] args) {
-    System.out.println("Hello world\n");
+public class TestProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world\n");
+    }
+
 }
